e1bfabda00e8cf54ab3aef189ad8820a46eaf014
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2024-11-26 00:49:04 +0000

    img added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-blue-100 font-bold underline">
+    <h1 className="text-3xl font-blue-900 text-center">
       Hello world!
     </h1>
   )
